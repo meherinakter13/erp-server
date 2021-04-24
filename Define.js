4d1bd9ed83df7d193c9a13fe3b948d7fa0f75bb9
Meherin Akter <meherinakter13@gmail.com>
2021-04-24 18:59:03 +0600

    login system

---
+++ Define.js
@@ -0,0 +1,11 @@
+const Define = {
+    //dept define
+    D_CAD: "CAD",
+    D_IE: "IE",
+    D_Marchandiser: "Marchandiser",
+    D_Buyer: "Buyer",
+    D_Production: "Production",
+    D_Sample: "Sample",
+}
+
+module.exports = Define
