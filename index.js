0a80f72f528d8bd8d3d121a1f65de2832213dfca
Meherin Akter <meherinakter13@gmail.com>
2021-04-24 19:05:05 +0600

    login

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const pool = mysql.createPool({
   connectionLimit: 15,
   host: 'localhost',
   user: 'root',
-<<<<<<< HEAD
   password: '17203028',
   database: 'erp-management'
 })
@@ -31,28 +30,6 @@ app.post('/addUser', (req, res) => {
       connection.release()
       if (!err) {
         res.send(`user with the name ${[userRecord.name]} has been added`)
-=======
-  password: '',
-  database: 'test'
-})
-
-// Add single user
-/**
- * 
- * eitai sign up/registration
- * 
- */
-app.post('/addUser', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected as id ${connection.threadId}`)
-
-    const userRecord = req.body
-
-    connection.query('INSERT INTO users SET ?', userRecord, (err, rows) => {
-      connection.release()
-      if (!err) {
-        res.send(`user with the name ${[userRecord.name]} has been added`)
       }
       else {
         console.log(err)
@@ -62,64 +39,6 @@ app.post('/addUser', (req, res) => {
   })
 })
 
-
-//loginv( email,password,department)
-app.post('/login', (req, res) => {
-  const { email, password, department } = req.body
-  const sql = `select * from users where email=? and password = ? and department = ?`
-  pool.query(sql, [email, password, department], (err, results) => {
-    if (err) {
-      res.send({ error: true, msg: 'user login failed' })
-    } else {
-      if (results[0]) {
-        res.send({ error: false, msg: 'user login success', data: results[0] })
-      } else {
-        res.send({ error: true, msg: 'user login failed' })
-      }
-    }
-  })
-})
-
-
-// get all user ()
-app.get('/user', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected as id ${connection.threadId}`)
-
-    connection.query('SELECT * from users', (err, rows) => {
-      connection.release()
-      if (!err) {
-        res.send(rows)
-      }
-      else {
-        console.log(err)
-      }
-    })
-  })
-})
-
-// get single user
-app.get('/user/:id', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected as id ${connection.threadId}`)
-
-    connection.query('SELECT * from users WHERE id = ?', [req.params.id], (err, rows) => {
-      connection.release()
-      if (!err) {
-        res.send(rows)
->>>>>>> 6f04af5cda435239f4768e8e72065c46b5f620e8
-      }
-      else {
-        console.log(err)
-      }
-    })
-    console.log(req.body);
-  })
-})
-
-<<<<<<< HEAD
 
 //login( email,password,department)
 app.post('/login', (req, res) => {
@@ -194,27 +113,6 @@ app.post('/login', (req, res) => {
 //     console.log(req.body);
 //   })
 // })
-=======
-// Update single user
-app.put('/updateUser', (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) throw err
-    console.log(`connected as id ${connection.threadId}`)
-
-    const { id, name, email, password, designation, department } = req.body;
-    connection.query('UPDATE users SET name = ?, email= ?, password = ?, designation = ?, department = ? WHERE id = ?', [id, name, email, password, designation, department], (err, rows) => {
-      connection.release()
-      if (!err) {
-        res.send(`user with the name ${name} has been updated`)
-      }
-      else {
-        console.log(err)
-      }
-    })
-    console.log(req.body);
-  })
-})
->>>>>>> 6f04af5cda435239f4768e8e72065c46b5f620e8
 
 
 
